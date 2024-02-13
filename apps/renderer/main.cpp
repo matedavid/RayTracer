3722ea2ea733493ef7872d3e9b1428b31099605d
matedavid <david.mate.lopez@gmail.com>
2024-02-13 01:51:46 +0100

    added json library

--- apps/renderer/main.cpp
+++ apps/renderer/main.cpp
@@ -1,3 +1,16 @@
-int main() {
+#include <iostream>
+
+#include "image_dumper.h"
+
+int main(int32_t argc, const char* argv[]) {
+    if (argc < 2) {
+        std::cout << "Usage: ./RayTracerRenderer <scene_file>.json\n";
+        return 1;
+    }
+
+    const std::string scene_file = argv[1];
+
+    // TODO:
+
     return 0;
 }
